d243dd77693b954d03d37049692fc430fa562b6f
lauralardies <lrodrlar@myuax.com>
2021-11-02 19:34:30 +0100

    Trabajo final, FINAL

--- Game.py
+++ Game.py
@@ -2,18 +2,24 @@ import random
 
 number = random.randint(0,99)
 
-print(number)
-
 print("Guess a number between 0 and 99:")
-guess = input("Write your guess:")
+guess = int(input("Write your guess: "))
+tries = 1
 
-while guess != str(number):  
-    if guess > str(number): 
-        print("The number is a bit lower than your guess, try again!")
+while guess != number:
+    if guess >= 0 and guess <= 99:
+        if guess > number: 
+            print("The number is a bit smaller than your guess, try again!")
+        elif guess < number:
+            print("The number is a bit bigger than your guess, try again!")
         print("Guess a number between 0 and 99:")
-        guess = input("Write your guess:")
-    elif guess < str(number):
-        print("The number is a bit higher than your guess, try again!")
+        guess = int(input("Write your guess: "))
+        tries = tries + 1
+    else:
         print("Guess a number between 0 and 99:")
-        guess = input("Write your guess:")
-print("Congratulations! You guessed the number correctly.")
+        guess = int(input("Write your guess: "))
+
+sTries = " try."
+if tries > 1:
+    sTries = " tries."
+print("Congratulations! You guessed the number correctly in " + str(tries) + sTries)
