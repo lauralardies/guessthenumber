943178af750ac96666754b19c83e7264fd539d05
lauralardies <lrodrlar@myuax.com>
2021-11-11 19:22:02 +0100

    intentos

--- Game.py
+++ Game.py
@@ -16,12 +16,12 @@ def showMenu ():
     return level
 
 def updateMin (guess, current):
-    if (current < guess):
+    if current < guess:
         return guess
     return current
 
 def updateMax (guess, current):
-    if (current > guess):
+    if current > guess:
         return guess
     return current
 
@@ -30,19 +30,24 @@ level = showMenu()
 if level == 1:
     number = random.randint(0,100)
     max = 100
+    max_tries = 20
 elif level == 2:
     number = random.randint(0,200)
     max = 200
+    max_tries = 30
 elif level == 3: 
     number = random.randint(0,300)
     max = 300
+    max_tries = 40
 elif level == 4:
     number = random.randint(0,400)
     max = 400
+    max_tries = 50
 else:
     print("Invalid level. Choosing easy level")
     number = random.randint(0,100)
     max = 100
+    max_tries = 20
 
 updated_max = max
 
@@ -51,6 +56,11 @@ guess = int(input("Write your guess: "))
 tries = 1
 
 while guess != number:
+    if tries == max_tries:
+        print ("You have ran out of tries. The number was " + str(number))
+        break
+    else:
+        print ("You have " + str(max_tries - tries) + " tries left")
     if guess >= 0 and guess <= max:
         if guess > number: 
             print("The number is a bit smaller than your guess, try again!")
@@ -59,7 +69,7 @@ while guess != number:
             print("The number is a bit bigger than your guess, try again!")
             updated_min = updateMin(guess, updated_min)
         hint = input("Do you require help? (Y/[N]): ")
-        if (str.upper(hint) == "Y"):
+        if str.upper(hint) == "Y":
             print ("The number is between " + str(updated_min) + " and " + str(updated_max))
         print("Guess a number between 0 and " + str(max) + ":")
         guess = int(input("Write your guess: "))
@@ -68,7 +78,8 @@ while guess != number:
         print("Guess a number between 0 and " + str(max) + ":")
         guess = int(input("Write your guess: "))
 
-sTries = " try."
-if tries > 1:
-    sTries = " tries."
-print("Congratulations! You guessed the number correctly in " + str(tries) + sTries)
+if guess == number:
+    sTries = " try."
+    if tries > 1:
+        sTries = " tries."
+    print("Congratulations! You guessed the number correctly in " + str(tries) + sTries)
