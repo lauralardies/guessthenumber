206398af2d774db9d1280c31b729697f7e35c6a4
lauralardies <lrodrlar@myuax.com>
2021-11-13 16:50:34 +0100

    FINAL

--- Game.py
+++ Game.py
@@ -5,12 +5,16 @@ NUMBER = 1
 MAX = 2
 MAX_TRIES = 3
 MIN = 4
+AI = 5
+END_GAME = 6
 
 def showMenu ():
     print ("1. Easy")
     print ("2. Intermediate")
     print ("3. Advanced")
     print ("4. Expert")
+    print ("5. AI")
+    print ("6. Exit")
     while True:
         level = input("Select your difficulty level: ")
         try:
@@ -18,12 +22,13 @@ def showMenu ():
         except:
             pass
         else:
-            if 1 <= level <= 4:
+            if 1 <= level <= 6:
                 break
     return level
 
-def selectLevel ():
-    level = showMenu()
+def selectLevel (level):
+    if level == 5:
+        level = random.randint(1,4)
     if level == 1:
         number = random.randint(0, 100)
         max = 100
@@ -74,36 +79,57 @@ def numberTries (value):
     if value > 1:
         return " tries"
     return " try"
-    
+
 def help ():
     hint = input("Do you require help? (Y/[N]): ")
     if str.upper(hint) == "Y":
         print("The number you are looking for is between " + str(updated_min) + " and " + str(updated_max))
 
-game_data = selectLevel()
-tries = 0
-updated_min = 0
-updated_max = game_data[MAX]
+def showScore(score):
+    print ("Score table:")
+    for score_item in score:
+        print ("Player : " + score_item[0] + ", level: " + str(score_item[1]) + ", solved in " + str(score_item[2]) + numberTries(score_item[2]))
 
+score =  []
 while True:
-    print ("Guess a number bewteen 0 and " + str(game_data[MAX]) + ":")
-    guess = chooseGuess()
-    tries += 1
-    score = []
-    if tries > game_data[MAX_TRIES]:
-        print("You have ran out of tries, the number was " + str(game_data[NUMBER]) + ".")
-    else:
-        print("You have " + str(game_data[MAX_TRIES] - tries) + numberTries(game_data[MAX_TRIES] - tries) + " left.")
-    if guess < game_data[NUMBER]:
-        print ("The number is a bit bigger than your guess, try again!")
-        updated_min = updateMin(guess, updated_min)
-        help()
-    elif guess > game_data[NUMBER]:
-        print("The number is a bit smaller than your guess, try again!")
-        updated_max = updateMax(guess, updated_max)
-        help()
-    else:
-        print("Congratulations! You guessed the number correctly in " + str(tries) + numberTries(tries) + ".")
-        name = input("Please, introduce your name: ")
-        score.append([name, game_data[LEVEL], tries])
+    level = showMenu()
+    if level == END_GAME:
+        showScore(score)
         break
+    game_data = selectLevel(level)
+    tries = 0
+    updated_min = 0
+    updated_max = game_data[MAX]
+    if level == AI:
+        print("The level the AI is playing is level " + str(game_data[LEVEL]))
+    while True:
+        if level == AI:
+            guess = random.randint(updated_min, updated_max)
+            print ("AI chose " + str(guess))
+        else:
+            print ("Guess a number bewteen 0 and " + str(game_data[MAX]) + ":")
+            guess = chooseGuess()
+        tries += 1
+        if tries > game_data[MAX_TRIES]:
+            print("You have ran out of tries, the number was " + str(game_data[NUMBER]) + ".")
+        else:
+            print("You have " + str(game_data[MAX_TRIES] - tries) + numberTries(game_data[MAX_TRIES] - tries) + " left.")
+        if guess == game_data[NUMBER]:
+            print("Congratulations! You guessed the number correctly in " + str(tries) + numberTries(tries) + ".")
+            if level == AI:
+                name = "Artificial Intelligence"
+            else:
+                name = input("Please, introduce your name: ")
+            score.append([name, game_data[LEVEL], tries])
+            break
+        else:
+            if guess < game_data[NUMBER]:
+                print ("The number is a bit bigger than your guess, try again!")
+                updated_min = updateMin(guess, updated_min)
+                if level != AI:
+                    help()
+            elif guess > game_data[NUMBER]:
+                print("The number is a bit smaller than your guess, try again!")
+                updated_max = updateMax(guess, updated_max)
+                if level != AI:
+                    help()
